9e2275b18bc7497655aa150faa109a5bb82b103e
gerardo9999 <56141101+gerardo9999@users.noreply.github.com>
2020-11-01 13:24:15 -0400

    t commit

--- public/js/app.js
+++ public/js/app.js
@@ -75635,7 +75635,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\Restaurante\tecnologia\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\gerar\OneDrive\Escritorio\Proyecto\Tecnologia\Tecnologia\resources\js\app.js */"./resources/js/app.js");
 
 
 /***/ })
